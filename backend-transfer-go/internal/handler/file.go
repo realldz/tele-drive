b9b30c8ac9f3f11d2735f855637c3357b43a7924
ScammerKute <31581520+realldz@users.noreply.github.com>
2026-06-10 02:49:04 +0700

    refactor(transfer): remove InitUpload and CompleteUpload routes

--- backend-transfer-go/internal/handler/file.go
+++ backend-transfer-go/internal/handler/file.go
@@ -74,9 +74,7 @@ func (h *FileHandler) RegisterRoutes(e *echo.Echo) {
 	// Authenticated endpoints
 	files.GET("/buffer-status", h.GetBufferStatus, authMiddleware)
 	files.POST("/upload", h.Upload, authMiddleware)
-	files.POST("/upload/init", h.InitUpload, authMiddleware)
 	files.POST("/upload/:fileId/chunk/:index", h.UploadChunk, authMiddleware)
-	files.POST("/upload/:fileId/complete", h.CompleteUpload, authMiddleware)
 	files.POST("/upload/:fileId/abort", h.AbortUpload, authMiddleware)
 	files.GET("/upload/:fileId/status", h.GetUploadStatus, authMiddleware)
 	files.POST("/:id/download-token", h.GenerateDownloadToken, authMiddleware)
